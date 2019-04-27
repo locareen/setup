fa22e29cf776553cd032eb9a581a020469a6d632
ksmt <ksmt4699@gmail.com>
2019-04-27 18:41:45 +0900

    apt-getを使用するように変更

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
-sudo apt update && sudo apt -y upgrade
+sudo apt-get update && sudo apt-get -y upgrade
 #ディレクトリ英語化
 ./eng-home-dir.sh
 #fcitx-Mozc導入
-sudo apt install -y fcitx fcitx-mozc mozc-utils-gui
+sudo apt-get install -y fcitx fcitx-mozc mozc-utils-gui
 #VSCodeリポジトリ追加
-sudo apt install -y curl
-sudo apt install -y apt-transport-https
+sudo apt-get install -y curl
+sudo apt-get install -y apt-transport-https
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -21,24 +21,24 @@ agi apt-transport-https ca-certificates
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 #GIMP2.8削除
-sudo apt purge -y gimp
+sudo apt-get purge -y gimp
 #Snapd追加
-sudo apt install spapd
+sudo apt-get install spapd
 #インストール
 sudo apt update
-sudo apt install -y google-chrome-stable
-sudo apt install -y git
-sudo apt install -y code
-sudo apt install -y vim
-sudo apt install -y virtualbox
-sudo apt install -y filezilla
-sudo apt install -y wine64
-sudo apt install -y winetricks
+sudo apt-get install -y google-chrome-stable
+sudo apt-get install -y git
+sudo apt-get install -y code
+sudo apt-get install -y vim
+sudo apt-get install -y virtualbox
+sudo apt-get install -y filezilla
+sudo apt-get install -y wine64
+sudo apt-get install -y winetricks
 winetricks allfonts
 sudo curl -o /usr/share/applications/wine.desktop https://raw.githubusercontent.com/wine-mirror/wine/5c2d6211f6590d3856dc9188593b3d3597c8b441/loader/wine.desktop
-sudo apt install -y docker-ce
-sudo apt install -y brasero
-sudo apt install -y fileziilla
+sudo apt-get install -y docker-ce
+sudo apt-get install -y brasero
+sudo apt-get install -y fileziilla
 sudo snap install gimp
 #Mozc辞書強化
 sudo systemctl start docker
@@ -57,5 +57,5 @@ wget http://download.opensuse.org/repositories/home:/codestation/xUbuntu_18.04/a
 wget http://download.opensuse.org/repositories/home:/codestation/xUbuntu_18.04/amd64/qcma_0.4.2_amd64.deb
 sudo dpkg -i ./*
 
-sudo apt purge -y blueman
-sudo apt autoremove
+sudo apt-get purge -y blueman
+sudo apt-get autoremove
