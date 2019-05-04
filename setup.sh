1e3124f1c63de80aac213f645d1aef996cd6ae90
ksmt4699 <ksmt4699@gmail.com>
2019-05-04 13:40:25 +0900

    sudoの削除,源真ゴシックインストールスクリプトの追加

--- setup.sh
+++ setup.sh
@@ -1,72 +1,94 @@
-#! /bin/sh
-sudo apt-get update && sudo apt-get -y upgrade
+#! /bin/bash
+#rootチェック
+if [ $UID != 0 ]; then
+        echo "You must run this as root."
+        exit 1
+fi
+apt-get update &&  apt-get -y upgrade
 #ディレクトリ英語化
 ./eng-home-dir.sh
 #fcitx-Mozc導入
-sudo apt-get install -y fcitx fcitx-mozc mozc-utils-gui
+apt-get install -y fcitx fcitx-mozc mozc-utils-gui
 #VSCodeリポジトリ追加
-sudo apt-get install -y curl
-sudo apt-get install -y apt-transport-https
+apt-get install -y curl
+apt-get install -y apt-transport-https
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo rm microsoft.gpg
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+rm microsoft.gpg
 #Virtualboxリポジトリ追加
-sudo sh -c 'echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian bionic contrib" > /etc/apt/sources.list.d/virtualbox.list'
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add  -
+sh -c 'echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian bionic contrib" > /etc/apt/sources.list.d/virtualbox.list'
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- |  apt-key add  -
 #Dockerリポジトリ追加
 agi apt-transport-https ca-certificates
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 #GIMP2.8削除
-sudo apt-get purge -y gimp
+apt-get purge -y gimp
 #Snapd追加
-sudo apt-get install spapd
+apt-get install spapd
 #Wineリポジトリ追加
-sudo dpkg --add-architecture i386
-sudo sh -c 'echo "deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main" > /etc/apt/sources.list.d/winehq.list'
+dpkg --add-architecture i386
+sh -c 'echo "deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main" > /etc/apt/sources.list.d/winehq.list'
 wget -q https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key
-sudo rm winehq.key
+apt-key add winehq.key
+rm winehq.key
 #インストール
-sudo apt update
-sudo apt-get install -y xterm
-sudo apt-get install -y git
-sudo apt-get install -y code
-sudo apt-get install -y vim
-sudo apt-get install -y virtualbox-6.0
-sudo apt-get install -y filezilla
-sudo apt-get install -y winehq-stable
-sudo apt-get install -y docker-ce
-sudo apt-get install -y cabextract
-sudo apt-get install -y brasero
-sudo snap install gimp
+apt update
+apt-get install -y xterm
+apt-get install -y git
+apt-get install -y code
+apt-get install -y vim
+apt-get install -y virtualbox-6.0
+apt-get install -y filezilla
+apt-get install -y winehq-stable
+apt-get install -y docker-ce
+apt-get install -y cabextract
+apt-get install -y brasero
+snap install gimp
 #Wine後処理
 cd "/usr/bin"
-sudo wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
-sudo chmod +x winetricks
+wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
 winetricks allfonts
-sudo curl -o /usr/share/applications/wine.desktop https://raw.githubusercontent.com/wine-mirror/wine/5c2d6211f6590d3856dc9188593b3d3597c8b441/loader/wine.desktop
+curl -o /usr/share/applications/wine.desktop https://raw.githubusercontent.com/wine-mirror/wine/5c2d6211f6590d3856dc9188593b3d3597c8b441/loader/wine.desktop
 #Mozc辞書強化
-sudo systemctl start docker
-sudo docker run -d --name mozc-ut2 shufo/mozc-ut2:bionic
-sudo docker cp mozc-ut2:/app/ ./deb/
-sudo docker rm mozc-ut2
+systemctl start docker
+docker run -d --name mozc-ut2 shufo/mozc-ut2:bionic
+docker cp mozc-ut2:/app/ ./deb/
+docker rm mozc-ut2
 cd ./deb
-sudo dpkg -i ./mozc-data_*.deb ./mozc-server_*.deb ./mozc-utils-gui_*.deb ./fcitx-mozc_*.deb
+dpkg -i ./mozc-data_*.deb ./mozc-server_*.deb ./mozc-utils-gui_*.deb ./fcitx-mozc_*.deb
 cd ../
-sudo rm -r ./deb
+rm -r ./deb
 #QCMA導入
 mkdir /tmp/qcma
 cd /tmp/qcma
 wget http://download.opensuse.org/repositories/home:/codestation/xUbuntu_18.04/amd64/libvitamtp5_2.5.9_amd64.deb
 wget http://download.opensuse.org/repositories/home:/codestation/xUbuntu_18.04/amd64/qcma-cli_0.4.2_amd64.deb
 wget http://download.opensuse.org/repositories/home:/codestation/xUbuntu_18.04/amd64/qcma_0.4.2_amd64.deb
-sudo dpkg -i ./*
-sudo rm -r /tmp/qcma
+dpkg -i ./*
+rm -r /tmp/qcma
 #Tear解消
-sudo mkdir /etc/X11/xorg.conf.d
-sudo echo -e 'Section "Device"\n   Identifier  "Intel Graphics"\n   Driver      "intel"\n   Option      "TearFree"    "true"\nEndSection' | sudo tee /etc/X11/xorg.conf.d/20-intel.conf > /dev/null
+mkdir /etc/X11/xorg.conf.d
+echo -e 'Section "Device"\n   Identifier  "Intel Graphics"\n   Driver      "intel"\n   Option      "TearFree"    "true"\nEndSection' |  tee /etc/X11/xorg.conf.d/20-intel.conf > /dev/null
+#GenShinGothicの導入
+mkdir /tmp/gsg && cd /tmp/gsg
+wget -q https://osdn.jp/downloads/users/8/8634/genshingothic-20150607.7z
+7z x *.7z
+mkdir /usr/share/fonts/truetype/GenShinGothic
+name="GenShinGothic"
+for i in "$name-ExtraLight.ttf" "$name-Light.ttf" "$name-Normal.ttf" "$name-Bold.ttf" "$name-Heavy.ttf"; do
+	mv $i /usr/share/fonts/truetype/GenShinGothic/
+done
+
+name="GenShinGothic-monospace"
+for i in "$name-ExtraLight.ttf" "$name-Light.ttf" "$name-Normal.ttf" "$name-Bold.ttf" "$name-Heavy.ttf"; do
+        mv $i /usr/share/fonts/truetype/GenShinGothic/
+done
+
+fc-cache -fv
 #blueman削除
-sudo apt-get purge -y blueman
-sudo apt-get autoremove
+apt-get purge -y blueman
+
+apt-get autoremove
