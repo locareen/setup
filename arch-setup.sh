d89d5fd15bde59109530bba9c1c7108936ee73cd
ksmt4699 <ksmt4699@gmail.com>
2019-06-19 15:17:21 +0900

    vbox-extの追加

--- arch-setup.sh
+++ arch-setup.sh
@@ -4,7 +4,7 @@ if [ $uid = 0 ]; then
 	exit 1
 fi
 mkdir /tmp/yay && cd /tmp/yay && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-yay -S openssl pulseaudio pavucontrol vim fcitx-mozc bash-completion chromium git xf86-video-intel os-prober p7zip virtualbox linux-headers visual-studio-code update-grub
+yay -S openssl pulseaudio pavucontrol vim fcitx-mozc bash-completion chromium git xf86-video-intel os-prober p7zip virtualbox linux-headers visual-studio-code update-grub virtualbox-ext-oracle
 echo -e 'Section "InputClass"\n Identifier "Keyboard Defaults"\n MatchIsKeyboard "yes"\n Option "XKbLayout" "jp"\n End Section' | sudo tee /etc/X11/xorg.conf.d/10-keyboard.conf > /dev/null
 echo -e 'KEYMAP=jp106' | sudo tee  /etc/vconsole.conf > /dev/null
 echo -e 'export GTK_IM_MODULE=fcitx\n export QT_IM_MODULE=fcitx\n export XMODIFIERS=" @im=fcitx"' | sudo tee  ~/.xprofile > /dev/null
